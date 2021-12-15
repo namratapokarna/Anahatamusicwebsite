38546b9b646a48426a4fbd5ec0ae302ab9c2594c
Namrata Pokarna <85691660+namratapokarna@users.noreply.github.com>
2021-12-15 14:57:46 +0530

    Add files via upload

---
+++ 10.js
@@ -0,0 +1,56 @@
+function totalIt() {
+    var input = document.getElementsByName("product");
+    var total = 0;
+    for (var i = 0; i < input.length; i++) {
+        if (input[i].checked) {
+            total += parseFloat(input[i].value);
+        }
+    }
+    document.getElementsByName("total")[0].value = "₹" + total.toFixed(2);
+}
+// form js
+
+
+$(document).ready(function() {
+
+    var navListItems = $('div.setup-panel div a'),
+        allWells = $('.setup-content'),
+        allNextBtn = $('.nextBtn');
+
+    allWells.hide();
+
+    navListItems.click(function(e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    allNextBtn.click(function() {
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+        $(".form-group").removeClass("has-error");
+        for (var i = 0; i < curInputs.length; i++) {
+            if (!curInputs[i].validity.valid) {
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('div.setup-panel div a.btn-primary').trigger('click');
+});
